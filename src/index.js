cf21cd262b143c06515c749e6c78b1e3b7230fdd
Heshan <mheshan27@gmail.com>
2022-08-11 23:41:26 +0530

    remove test comments

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import App from "./Route/App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App /> // this is app 
+    <App />
   </React.StrictMode>
 );
-
-
