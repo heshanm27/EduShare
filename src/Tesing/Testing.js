5842f5502b1e9b0ab205378ad94b35c10958a0f7
Ruchira Ranasinghe <ranasingheruchira@gmail.com>
2022-08-31 20:20:01 +0530

    test

--- src/Tesing/Testing.js
+++ src/Tesing/Testing.js
@@ -1,6 +1,7 @@
 // this is testing 1
-// react is best frame work 
-// we are team 
+// react is best frame work
+// we are team
 /*React makes it painless to create interactive UIs. Design simple views for each state in your application,
  and React will efficiently update and render just the right components when your data changes.*/
 /*Declarative views make your code more predictable and easier to the debug.*/
+//h
