3e004950283a98ae3c8065a0ae2afca6716b2052
IT20299620 <86467727+IT20299620@users.noreply.github.com>
2022-09-05 16:03:02 +0530

    corection of code

--- src/Tesing/Testing.js
+++ src/Tesing/Testing.js
@@ -25,5 +25,8 @@
 /*React components implement a render() method that takes input data and returns what to display. This example uses an XML-like syntax called JSX.
  Input data that is passed into the component can be accessed by render() via this.props.*/
 
+/*JSX is optional and not required to use React.
+ Try the Babel REPL to see the raw JavaScript code produced by the JSX compilation step.*/
+
 
 
