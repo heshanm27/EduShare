ad74eaab392247186b9e7c1b90639b2de1bdd5c4
Ruchira Ranasinghe <it20068028@my.sliit.lk>
2022-09-14 10:17:30 +0530

    Edited the feed
    added thumbnail to the feed

--- src/Tesing/Testing.js
+++ src/Tesing/Testing.js
@@ -92,3 +92,28 @@ foreign woking opportunities to build a good career protfoloio.
    This example uses state to track the current list of items as well as the text that the user has entered.
    Although event handlers appear to be rendered inline,
    they will be collected and implemented using event delegation.*/
+
+/*Using props and state, we can put together a small Todo application.
+   This example uses state to track the current list of items as well as the text that the user has entered.
+   Although event handlers appear to be rendered inline,
+   they will be collected and implemented using event delegation.*/
+
+/*Using props and state, we can put together a small Todo application.
+   This example uses state to track the current list of items as well as the text that the user has entered.
+   Although event handlers appear to be rendered inline,
+   they will be collected and implemented using event delegation.*/
+
+/*Using props and state, we can put together a small Todo application.
+   This example uses state to track the current list of items as well as the text that the user has entered.
+   Although event handlers appear to be rendered inline,
+   they will be collected and implemented using event delegation.*/
+
+/*Using props and state, we can put together a small Todo application.
+   This example uses state to track the current list of items as well as the text that the user has entered.
+   Although event handlers appear to be rendered inline,
+   they will be collected and implemented using event delegation.*/
+
+/*Using props and state, we can put together a small Todo application.
+   This example uses state to track the current list of items as well as the text that the user has entered.
+   Although event handlers appear to be rendered inline,
+   they will be collected and implemented using event delegation.*/
